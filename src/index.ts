408e6103a9d63996d7b1d871cb59a617382b7603
Valery Zinchenko <minicablestone@gmail.com>
2024-11-30 19:28:55 +0300

    turn into library

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Act from "./Act"
+import Events from "./Events"
+import { WebInflator } from "./Inflator"
+import Proton from "./Proton"
+
+export { Proton, Events, Act, WebInflator }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
+import rollupTS2 from "rollup-plugin-typescript2"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [rollupTS2({ abortOnError: false, check: false, useTsconfigDeclarationDir: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -14,10 +15,23 @@ export default defineConfig({
     outDir: "build",
     sourcemap: true,
     modulePreload: false,
+
+    emptyOutDir: true,
+
+    rollupOptions: {
+      external: [/(?!react-modal-global)react.*/],
+      treeshake: true
+    },
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      formats: ["es"],
+      fileName: "index"
+    }
   },
   esbuild: {
+    treeShaking: true,
     minifyIdentifiers: false,
     minifyWhitespace: false,
-    ignoreAnnotations: true
-  }
+    ignoreAnnotations: false,
+  },
 })
