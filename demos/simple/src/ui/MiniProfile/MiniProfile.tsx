ce22de9e6229e3a553ece735d7dd043782d4296f
Valery Zinchenko <minicablestone@gmail.com>
2024-12-05 20:48:18 +0300

    11

--- demos/simple/src/ui/MiniProfile/MiniProfile.tsx
+++ demos/simple/src/ui/MiniProfile/MiniProfile.tsx
@@ -12,20 +12,27 @@ interface MiniProfileProps {
 
 function MiniProfile(this: Proton.Shell, props: MiniProfileProps) {
   const user = Events.State.from(props.user)
+  const userAvatar = user.$.avatar
+
+  const inputValue = new Events.State("")
+  const inputMounted = new Events.State(false)
+
+  inputValue[Symbol.subscribe](value => userAvatar.set(value))
+  inputValue[Symbol.subscribe](v => console.log(v))
 
   this.view.set(
     <div className="mini-profile">
       <div className="mini-profile__profile">
-        <div className="mini-profile__letter" mounted={user.$.avatar.is(it => it == null)}>
-          <ColoredLetter letter={user.to(user => user.firstName[0])} />
-        </div>
-        <img className="mini-profile__avatar" src={user.$.avatar.guard(it => it != null)} alt="avatar" />
+        <button className="mini-profile__letter" type="button" on={{ click: () => inputMounted.set(true) }}>
+          {/* <ColoredLetter letter={user.to(user => user.firstName[0])} /> */}
+          asd
+        </button>
+        <input value={inputValue} mounted={inputMounted} />
+        <img className="mini-profile__avatar" src={userAvatar.required} alt="avatar" />
         <div className="mini-profile__info">
           <div className="mini-profile__name">{user.$.firstName} {user.$.lastName.to(it => it[0].toUpperCase())}</div>
           <div className="mini-profile__email">{user.$.email}</div>
         </div>
-        {/* <a className="ghost" href="/user/profile/settings" /> */}
-
       </div>
     </div>
   )
