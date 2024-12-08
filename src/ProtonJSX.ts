f38732f033eebbce8cd9496782979242bc4a4b30
Valery Zinchenko <minicablestone@gmail.com>
2024-12-08 18:16:15 +0300

    remove redundant code

--- src/ProtonJSX.ts
+++ src/ProtonJSX.ts
@@ -1,8 +1,6 @@
 import { Primitive } from "type-fest"
 
 
-type DocumentNode = Node
-
 namespace ProtonJSX {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
   export interface Node {
@@ -40,52 +38,6 @@ namespace ProtonJSX {
     return new Component(type, props, children, childrenExtrinsic)
   }
   export const Fragment = Symbol.for("Proton.Fragment")
-
-
-
-
-
-
-
-
-
-
-
-
-
-  export interface IntrinsicElement<Type extends keyof never = keyof never> {
-    type: Type
-    props: Record<keyof never, unknown> | null
-    children: Element | Element[] | null
-  }
-
-  export abstract class IntrinsicEvaluator {
-    public abstract evaluate(intrinsic: IntrinsicElement<keyof never>): unknown
-  }
-  export abstract class PrimitiveEvaluator {
-    public abstract evaluate(primitive: Primitive): unknown
-  }
-
-  export class HTMLIntrinsicEvaluator extends IntrinsicEvaluator {
-    constructor(
-      protected readonly transformIntrinsic: <Type extends keyof HTMLElementTagNameMap>(input: IntrinsicElement<Type>) => typeof input,
-      protected readonly transformElement: <T extends HTMLElement>(output: T) => T
-    ) { super() }
-
-    public override evaluate<Type extends keyof HTMLElementTagNameMap>(intrinsic: IntrinsicElement<Type>): HTMLElementTagNameMap[Type] {
-      intrinsic = this.transformIntrinsic(intrinsic)
-
-      const element = document.createElement(intrinsic.type)
-      Object.assign(element, intrinsic.props)
-
-      return this.transformElement(element)
-    }
-  }
-  export class HTMLPrimitiveEvaluator extends PrimitiveEvaluator {
-    public override evaluate(primitive: Primitive): DocumentNode {
-      return document.createTextNode(String(primitive))
-    }
-  }
 }
 
 export default ProtonJSX
