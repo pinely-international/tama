4035c8147a28fb3f43f10dcb6ddb225c5e2c8c1e
Valery Zinchenko <minicablestone@gmail.com>
2024-12-11 22:57:00 +0300

    21

--- demos/simple/src/tictactoe/tictactoe.tsx
+++ demos/simple/src/tictactoe/tictactoe.tsx
@@ -63,7 +63,9 @@ export default function Game() {
   const currentMove = new Events.State(0)
 
   const xIsNext = currentMove.to(it => it % 2 === 0)
-  const currentSquares = currentMove.to(it => history.get()[it])
+  const currentSquares = Events.State.compute([currentMove, history], (currentMove, history) => {
+    return history[currentMove]
+  })
 
   function onPlay(nextSquares) {
     const nextHistory = [...history.get().slice(0, currentMove.get() + 1), nextSquares]
