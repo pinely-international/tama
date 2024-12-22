5465c904112532ab3333be92308479ef07a32582
Valery Zinchenko <minicablestone@gmail.com>
2024-12-22 15:55:07 +0300

    1

--- src/Events.ts
+++ src/Events.ts
@@ -1,11 +1,8 @@
-import { AccessorGet, AccessorSet } from "./Accessor"
-import Act from "./Act"
-import Guarded from "./Guarded"
 import Null from "./Null"
 import Observable, { Subscriptable, Unsubscribe } from "./Observable"
 import Proton from "./Proton"
 
-const finalization = new FinalizationRegistry<() => void>(unsubscribe => unsubscribe())
+
 
 class Events<EventMap extends Record<EventName, unknown>, EventName extends keyof EventMap = keyof EventMap> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -73,182 +70,6 @@ namespace Events {
    * Shorthand for `Events.Messager<void>`. You can use it for better semantics.
    */
   export class Notifier extends Events.Messager<void> { }
-  export class State<T> {
-    static of<T>(items: (T | State<T>)[]): State<T>[] {
-      return items.map(State.from)
-    }
-
-    static from<T>(item: T | State<T>): State<T> {
-      if (item instanceof State) return item
-
-      return new State(item)
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    static compute<const States extends State<any>[], U>(states: States, predicate: (...values: { [K in keyof States]: ReturnType<States[K]["get"]> }) => U): State<U> {
-      const values = states.map(state => state.get())
-
-      const computed = new State(predicate(...values as never))
-
-      states.forEach((state, index) => {
-        state[Symbol.subscribe](value => {
-          if (values[index] === value) return
-
-          values[index] = value
-          computed.set(predicate(...values as never))
-        })
-      })
-
-      return computed
-    }
-
-    private readonly callbacks = new Set<(value: T) => void>()
-    private value: T
-
-    constructor(initialValue: T) {
-      this.value = initialValue
-
-      this.boundGet = (() => this.get()) as never
-      this.boundGet[Symbol.subscribe] = this[Symbol.subscribe]
-    }
-
-
-    get(): T { return this.value }
-    set(newValue: T | ((current: T) => T)): void {
-      const value = newValue instanceof Function ? newValue(this.value) : newValue
-
-      this.value = value
-      this.dispatch(value)
-    }
-
-
-    sets<U>(other: AccessorSet<T | U>): Unsubscribe
-    sets(callback: (value: T) => void): Unsubscribe
-    sets<U>(arg: AccessorSet<T | U> | ((value: T) => void)): Unsubscribe {
-      const set = arg instanceof Function ? arg : arg.set
-
-      return this[Symbol.subscribe](value => set(value))
-    }
-    copy(other: AccessorGet<T>) { this.set(other.get()) }
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    private readonly $ProxyCache: Partial<Record<keyof T, unknown>> = {}
-    readonly $ = new Proxy(this, {
-      get: (target, key) => {
-        const cached = this.$ProxyCache[key as keyof T]
-        if (cached != null) return cached
-
-        const targetValueItem = target.value[key as keyof T]
-        if (targetValueItem instanceof Function) {
-          const method = (...args: unknown[]) => {
-            const result = targetValueItem.apply(target.value, args)
-            // Method resulting with itself usually means it was updated.
-            if (result === target.value) {
-              target.dispatch(target.value)
-              return target
-            }
-
-            const predicate = (value: T) => targetValueItem.apply(value, args)
-            const fork = new State(result)
-            // Follows `this.to` method implementation from here.
-            this[Symbol.subscribe](value => {
-              const newValue = predicate(value)
-              newValue !== fork.value && fork.set(newValue)
-            })
-            return fork
-          }
-          this.$ProxyCache[key as keyof T] = method
-
-          return method
-        }
-
-        const property = target.to(value => value[key as keyof T])
-        this.$ProxyCache[key as keyof T] = property
-
-        return property
-      }
-    }) as unknown as { [K in keyof T]-?: T[K] extends (...args: infer Args) => infer Return ? (...args: Args) => State<Return> : State<T[K]> }
-
-    get it() { return this.value }
-    set it(value: T) { this.set(value) }
-
-    to<U>(predicate: (value: T) => U): State<U> {
-      const fork = new State(predicate(this.value))
-      this[Symbol.subscribe](value => {
-        const newValue = predicate(value)
-        newValue !== fork.value && fork.set(newValue)
-      })
-      return fork
-    }
-
-    from(predicate: (value: T) => T): State<T> {
-      const fork = new State(this.value)
-      const set = fork.set
-      fork.set = value => set.call(fork, value instanceof Function ? predicate(value(fork.value)) : predicate(value))
-      this.sets(fork)
-      return fork
-    }
-
-    fork() { new State(this.get()) }
-    clone() {
-      const cloned = new State(this.get())
-      this.sets(cloned)
-      return cloned
-    }
-
-    readonly(): StateReadonly<T> {
-      return { get: () => this.get(), [Symbol.subscribe]: next => this[Symbol.subscribe](next) }
-    }
-
-    writeonly(): StateWriteonly<T> {
-      return { set: v => this.set(v) }
-    }
-
-    is(predicate: (value: T) => boolean): StateReadonly<boolean>
-    is<U extends T>(predicate: (value: T) => value is U): StateReadonly<boolean> {
-      return { get: () => predicate(this.get()), [Symbol.subscribe]: next => this[Symbol.subscribe](value => next(predicate(value))) }
-    }
-    readonly isNullish: StateReadonly<boolean> = this.is(value => value == null)
-    readonly isNotNullish: StateReadonly<boolean> = this.is(value => value != null)
-
-    guard<U extends T>(predicate: (value: T) => boolean): Guarded<U, T> & StateReadonly<T>
-    guard<U extends T>(predicate: (value: T) => value is U): Guarded<U, T> & StateReadonly<T> {
-      const guardedState = this.readonly() as Guarded<U, T> & StateReadonly<T>
-      guardedState.valid = predicate
-
-      return guardedState
-    }
-    readonly nullable: Guarded<T | null | undefined, T | null | undefined> & StateReadonly<T> = this.guard(value => value == null)
-    readonly nonNullable: Guarded<T & {}, T> & StateReadonly<T & {}> = this.guard(value => value != null) as never
-    readonly required: Guarded<T & {}, T> & StateReadonly<T & {}> = this.nonNullable
-
-
-    private boundGet: ((() => T) & Observable<T>)
-    private boundSet = (newValue: T | ((current: T) => T)) => this.set(newValue)
-
-
-    private dispatch(value: T) {
-      this.callbacks.forEach(callback => callback(value))
-    }
-
-    [Symbol.reveal]() { return this.value }
-    [Act.descriptor](): PropertyDescriptor {
-      return { get: this.boundGet, set: this.boundSet }
-    }
-
-    *[Symbol.iterator]() {
-      yield this.boundGet
-      yield this.boundSet
-    }
-    [Symbol.subscribe](next: (value: T) => void) {
-      this.callbacks.add(next)
-
-      const unsubscribe = () => void this.callbacks.delete(next)
-      finalization.register(this, unsubscribe)
-
-      return { unsubscribe }
-    }
-  }
 
   export class Index<T> {
     private array: T[]
@@ -292,7 +113,7 @@ namespace Events {
     orderOf(item: T): Observable<number> {
       const next = () => this.nonNullableArray().indexOf(item)
 
-      const indexState = new State(next())
+      const indexState = new Flow(next())
       this.on("null").subscribe?.(() => indexState.set(next))
       this.on("replace").subscribe?.(() => indexState.set(next))
       return indexState.readonly()
@@ -320,10 +141,10 @@ namespace Events {
     readonly EMPTY = Null.OBJECT
   }
 
-  export class StateIndex<T> extends Index<State<T>> {
-    constructor(init: Iterable<T> | Observable<Iterable<T>> | State<Iterable<T>>) {
-      if (init instanceof State) {
-        super(State.of([...init.get()]))
+  export class StateIndex<T> extends Index<Flow<T>> {
+    constructor(init: Iterable<T> | Observable<Iterable<T>> | Flow<Iterable<T>>) {
+      if (init instanceof Flow) {
+        super(Flow.of([...init.get()]))
 
         init[Symbol.subscribe](items => this.replace([...items]))
       } else if (Symbol.iterator in init === false) {
@@ -331,16 +152,13 @@ namespace Events {
 
         init[Symbol.subscribe](items => this.replace([...items]))
       } else {
-        super(State.of([...init]))
+        super(Flow.of([...init]))
       }
     }
 
-    override push(item: T | State<T>) { return super.push(State.from(item)) }
-    override replace(items: (T | State<T>)[]) { return super.replace(State.of(items)) }
+    override push(item: T | Flow<T>) { return super.push(Flow.from(item)) }
+    override replace(items: (T | Flow<T>)[]) { return super.replace(Flow.of(items)) }
   }
 }
 
 export default Events
-
-type StateReadonly<T> = Observable<T> & AccessorGet<T>
-type StateWriteonly<T> = { set(value: T | ((value: T) => T)): void }
