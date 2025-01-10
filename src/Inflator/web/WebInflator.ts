6371905a2a7032b5f2522e8e78043ee884aa42d8
Valery Zinchenko <minicablestone@gmail.com>
2025-01-10 21:34:25 +0300

    resolve replacement 13

--- src/Inflator/web/WebInflator.ts
+++ src/Inflator/web/WebInflator.ts
@@ -291,8 +291,10 @@ class WebInflator extends Inflator {
     let lastAnimationFrame = -1
 
     const schedule = (nextView: Node) => {
-      // @ts-expect-error by design.
-      nextView = nextView?.shell?.getView?.() ?? nextView
+      if (nextView instanceof WebComponentPlaceholder === false) {
+        // @ts-expect-error by design.
+        nextView = nextView?.shell?.getView?.() ?? nextView
+      }
       currentView = resolveReplacement(currentView)
 
       if ("replaceWith" in currentView && currentView.replaceWith instanceof Function) {
