cd064487c8bd760e0e1d344530840d80aeffbcc3
Valery Zinchenko <minicablestone@gmail.com>
2025-01-10 19:19:15 +0300

    resolve replacement 3

--- src/Inflator/web/WebInflator.ts
+++ src/Inflator/web/WebInflator.ts
@@ -356,8 +356,5 @@ export default WebInflator
 
 
 function resolveReplacement(value: any): any {
-  if (value == null) return value
-  if (value === value.replacedWith) return value
-
-  return resolveReplacement(value.replacedWith)
+  return resolveReplacement(value.replacedWith) ?? value
 }
