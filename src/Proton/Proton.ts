1a15a102fa643122ed53b2becb4efbbea04a183b
Valery Zinchenko <minicablestone@gmail.com>
2025-01-10 16:07:18 +0300

    fix export 4

--- src/Proton/Proton.ts
+++ src/Proton/Proton.ts
@@ -1,14 +1,11 @@
 import { ProtonLazy } from "./ProtonLazy"
-import { ProtonList } from "./ProtonList"
+import { ProtonList, ProtonListWebInflator } from "./ProtonList"
 import { ProtonShell } from "./ProtonShell"
-
-export * from "./ProtonLazy"
-export * from "./ProtonList"
-export * from "./ProtonShell"
 
 const Proton = {
   Lazy: ProtonLazy,
   List: ProtonList,
+  ListWebInflator: ProtonListWebInflator,
   Shell: ProtonShell,
 }
 export default Proton
