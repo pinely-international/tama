554fd1739efd036ba953d089059c2bfafd3a44e5
Valery Zinchenko <minicablestone@gmail.com>
2025-01-17 20:04:08 +0300

    drop `ProtonList` support in favor of observable iterables

--- src/Proton/Proton.d.ts
+++ src/Proton/Proton.d.ts
@@ -1,11 +1,10 @@
 import { ProtonDynamic as Dynamic } from "./ProtonDynamic"
 import { ProtonLazy as Lazy } from "./ProtonLazy"
-import { ProtonList as List, ProtonListWebInflator as ListWebInflator } from "./ProtonList"
 import { ProtonShell as Shell } from "./ProtonShell"
 
 
 declare namespace Proton {
-  export { Lazy, Dynamic, List, Shell, ListWebInflator }
+  export { Lazy, Dynamic, Shell }
 }
 
 export default Proton
--- src/Proton/Proton.ts
+++ src/Proton/Proton.ts
@@ -1,6 +1,5 @@
 import { ProtonDynamic as Dynamic } from "./ProtonDynamic"
 import { ProtonLazy as Lazy } from "./ProtonLazy"
-import { ProtonList as List, ProtonListWebInflator as ListWebInflator } from "./ProtonList"
 import { ProtonShell as Shell } from "./ProtonShell"
 
-export default { Lazy, Dynamic, List, Shell, ListWebInflator }
+export default { Lazy, Dynamic, Shell }
