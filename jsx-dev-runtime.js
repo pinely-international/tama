1c288aed0ddcd7f37542449d7550f5f664f2775d
Valery Zinchenko <minicablestone@gmail.com>
2025-01-25 12:47:16 +0300

    fix `jsx-dev-runtime.js` 5

--- jsx-dev-runtime.js
+++ jsx-dev-runtime.js
@@ -1,4 +1,5 @@
-export { Fragment, jsx } from "./build/jsx-runtime"
+import { jsx } from "./build/jsx-runtime"
+export { Fragment } from "./build/jsx-runtime"
 
 export function jsxDEV(type, props, children, isStaticChildren, source, self) {
   return jsx(type, props, children)
