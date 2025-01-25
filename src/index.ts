c0960f95d0e9fac71c52fc089a33d76ae0cde6da
Valery Zinchenko <minicablestone@gmail.com>
2025-01-25 14:25:14 +0300

    export `ProtonSwitchWebInflator`

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import "./REQUIRED_SYMBOLS"
 export { default as WebInflator } from "./Inflator/web/WebInflator"
 
 export { default as Proton } from "./Proton/Proton"
+
+export { ProtonSwitchWebInflator } from "./Proton/ProtonSwitch"
