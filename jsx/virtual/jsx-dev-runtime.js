8fbde27d16bb19c1d4a418bd63f6f08e1d7ab4a3
Valery Zinchenko <minicablestone@gmail.com>
2025-02-26 16:03:18 +0300

    Allow choosing JSX resolver 2

--- jsx/virtual/jsx-dev-runtime.js
+++ jsx/virtual/jsx-dev-runtime.js
@@ -1,5 +1,5 @@
-import { jsx } from "../build/jsx-runtime"
-export { Fragment } from "../build/jsx-runtime"
+import { jsx } from "../../build/jsx-runtime"
+export { Fragment } from "../../build/jsx-runtime"
 
 export function jsxDEV(type, props, children, isStaticChildren, source, self) {
   return jsx(type, props, children)
