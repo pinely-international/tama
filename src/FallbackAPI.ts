2ebb5ccb4225801d19ed5976f3f4a986f46dfb67
Valery Zinchenko <minicablestone@gmail.com>
2025-07-20 21:12:24 +0300

    add `FallbackAPI`

--- src/FallbackAPI.ts
+++ src/FallbackAPI.ts
@@ -1,3 +1,26 @@
-class FallbackAPI { }
+import { State } from "@denshya/reactive"
 
-export default FallbackAPI
+type FallbackResult = Error | Promise<unknown> | true | null
+
+export class FallbackAPI<ErrorView = unknown, PendingView = unknown> {
+  readonly error = new State<Error | null>(null)
+  readonly pending = new State<boolean | Promise<unknown>>(false)
+
+  /** @internal */
+  readonly result = State.combine([this.error, this.pending], (error, pending) => error ?? (pending || null))
+
+  constructor(public views: { error: ErrorView, pending: PendingView }) { }
+
+  resolve<T>(value: T, result: FallbackResult): T | ErrorView | PendingView {
+    if (result == null) return value
+    if (result === true) return this.views.pending
+
+    if (result instanceof Error) return this.views.error
+    if (result instanceof Promise) {
+      result.then(() => this.pending.set(false))
+      return this.views.pending
+    }
+
+    throw new TypeError("Unreachable code, something went wrong during Proton Fallback Resolution", { cause: this })
+  }
+}
