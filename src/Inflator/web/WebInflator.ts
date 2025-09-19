f2e80da97b50fadff2ae66f0531ba197a8c1b1f2
Valery Zinchenko <minicablestone@gmail.com>
2025-09-19 14:37:26 +0200

    refactor: extract helpers

--- src/Inflator/web/WebInflator.ts
+++ src/Inflator/web/WebInflator.ts
@@ -12,7 +12,7 @@ import { isIterable, isJSX, isRecord } from "@/utils/testers"
 import WebNodeBinding from "@/utils/WebNodeBinding"
 
 import { NAMESPACE_MATH, NAMESPACE_SVG } from "./consts"
-import { nonGuard } from "./helpers"
+import { iterableOf, nonGuard, onDemandRef } from "./helpers"
 
 import Inflator from "../Inflator"
 
@@ -452,28 +452,3 @@ class WebInflator extends Inflator {
 
 export default WebInflator
 
-function iterableOf(object: object) {
-  if (Symbol.iterator in object) return object
-  if ("get" in object && object.get instanceof Function) {
-    const value = object.get()
-    if (Symbol.iterator in value) return value
-  }
-
-  throw new TypeError("Unreachable code reached during extract of iterable from observable")
-}
-
-
-
-/**
- * Creates reference only when it's first accessed.
- */
-function onDemandRef<T>(factory: () => T) {
-  let value: T | null = null
-
-  return {
-    get current() {
-      if (value === null) value = factory()
-      return value
-    }
-  }
-}
--- src/Inflator/web/helpers.ts
+++ src/Inflator/web/helpers.ts
@@ -1,1 +1,28 @@
 export const nonGuard = (value: unknown) => value
+
+
+export function iterableOf(object: object) {
+  if (Symbol.iterator in object) return object
+  if ("get" in object && object.get instanceof Function) {
+    const value = object.get()
+    if (Symbol.iterator in value) return value
+  }
+
+  throw new TypeError("Unreachable code reached during extract of iterable from observable")
+}
+
+
+
+/**
+ * Creates reference only when it's first accessed.
+ */
+export function onDemandRef<T>(factory: () => T) {
+  let value: T | null = null
+
+  return {
+    get current() {
+      if (value === null) value = factory()
+      return value
+    }
+  }
+}
