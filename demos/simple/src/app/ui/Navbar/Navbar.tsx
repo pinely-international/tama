7d853df9ff282d813f38da14aed3921c6a5e1128
Valery Zinchenko <minicablestone@gmail.com>
2025-10-21 16:34:51 +0200

    1

--- demos/simple/src/app/ui/Navbar/Navbar.tsx
+++ demos/simple/src/app/ui/Navbar/Navbar.tsx
@@ -9,18 +9,8 @@ function Navbar() {
       <NavLink className="navbar__link" to="/">Home</NavLink>
       <NavLink className="navbar__link" to="/documentation">Documentation</NavLink>
 
-      <NavLink className="navbar__link" to="/circles">Circles</NavLink>
-
       <NavLink className="navbar__link" to="/profile">Profile</NavLink>
-      <NavLink className="navbar__link" to="/blog">Blog</NavLink>
-      <NavLink className="navbar__link" to="/messenger">Messenger</NavLink>
       <NavLink className="navbar__link" to="/market">Market</NavLink>
-      <NavLink className="navbar__link" to="/orders">Orders</NavLink>
-
-      <NavLink className="navbar__link" to="/products-table">Products Table</NavLink>
-      <NavLink className="navbar__link" to="/tictactoe">Tic-Tac-Toe</NavLink>
-      <NavLink className="navbar__link" to="/lobby">Lobby</NavLink>
-      <NavLink className="navbar__link" to="/frostpunk">Frostpunk 1</NavLink>
     </div>
   )
 }
