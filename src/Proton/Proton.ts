2ec6d7ef8b5884b1b23ba2c925d947da2b49102a
Valery Zinchenko <minicablestone@gmail.com>
2025-10-21 19:05:13 +0200

    feat: add `Proton.Ref`

--- src/Proton/Proton.ts
+++ src/Proton/Proton.ts
@@ -1,5 +1,6 @@
 import { ProtonDynamic as Dynamic } from "./ProtonDynamic"
 import { ProtonLazy as Lazy } from "./ProtonLazy"
+import { ProtonRef as Ref } from "./ProtonRef"
 import { ProtonSwitch as Switch } from "./ProtonSwitch"
 
-export default {Lazy, Dynamic, Switch}
+export default { Lazy, Dynamic, Switch, Ref }
