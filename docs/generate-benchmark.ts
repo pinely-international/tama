38ca71235c4cbc23679ee2e958b22d075c9b9fae
Valery Zinchenko <minicablestone@gmail.com>
2025-12-10 14:36:31 +0100

    docs: disable benchmarks

--- docs/generate-benchmark.ts
+++ docs/generate-benchmark.ts
@@ -5,10 +5,11 @@ import { run } from "../spec/bench"
 const converter = new Converter
 
 console.log("<pre className=\"asd\">")
-await run({
-  print: input => {
-    console.log(converter.toHtml(input).replace(/style="color:(.*?)"/g, `style={{ color: "$1" }}`).replace(/\s{2,}/g, v => "&nbsp;".repeat(v.length)))
-  }
-})
+console.log("temporary disabled")
+// await run({
+//   print: input => {
+//     console.log(converter.toHtml(input).replace(/style="color:(.*?)"/g, `style={{ color: "$1" }}`).replace(/\s{2,}/g, v => "&nbsp;".repeat(v.length)))
+//   }
+// })
 console.log("</pre>")
 
